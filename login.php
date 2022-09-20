d0424a0613744a648b701c80ecfcdea16cd098a1
CeciiBloise <bloisececii@gmail.com>
2022-09-20 12:12:14 -0300

    Estoy haciendo el login con la base de datos

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+//variables que van a recibir valor por teclado para la validacion
+$usuario-$_POST['usuario'];
+$contraseña-$_POST['contraseña'];
+
+session_start();
+
+$_SESSION  ['usuario']-$usuario;
+
+include('bd.php');
+
+$consulta="SELECT*FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_nun_rows($resultado);
+
+if($fila){
+    header("location:home.php");
+}else{
+    ?>
+    <?php
+    include("index.php");
+    ?>
+    <h1>Hay un error en la autotentificacion</h1>
+}
+
+mysqli_free_result($resultado);
+mysqli_close($conexion);
