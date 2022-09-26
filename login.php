5ee6df9fe56be710f6f3be344296864da51f0b11
CeciiBloise <bloisececii@gmail.com>
2022-09-26 10:49:03 -0300

    ANDA EL LOGIN, solo tengo que restringir los volver atras

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
-$dbname = "test"; //nombre de la base de datos
+$dbname = "proyecto_trenes"; //nombre de la base de datos
 
 $conexion = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //Condicional por si no hay conexion que muestre el error
@@ -17,17 +17,31 @@ $contraseña = $_POST["contraseñaa"];
 
 //Consulta que le va hacer al SQL, selecciona todo de la tabla login
 //y buscara coincidencia entre el usuario y la contrasena ingresados con la tabla del SQL
-$query =mysqli_query($conexion, "SELECT * FROM login WHERE usuario = '".$usuario."' and contraseña ='".$contraseña."'" );
+$query =mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '".$usuario."' and contraseña ='".$contraseña."'" );
 //Una vez encontra la coincidencia nos da como resultado una fila, un numero
-$nro = mysqli_num_rows($query);
+//$nro = mysqli_num_rows($query);
+$nro = mysqli_fetch_array($query); 
 
 //Permite el ingreso o la redirecciona al login
-if($nro == 1)
+if($nro['id_cargo'] == 1) //administrador general
 {
  header("Location: Paginas\Inicio.html");
  //echo "Bienvenido:" .$usuario;
 }
-else if ($nro == 0)
+
+if($nro['id_cargo'] == 2) //administrador personal
+{
+ header("Location: Paginas\Servicio_medico\Servicio_medico.html");
+ //echo "Bienvenido:" .$usuario;
+}
+
+if($nro['id_cargo'] == 3) //mecanico
+{
+ header("Location: Paginas\Servicio_medico\Paginas_de_servicio_medico\1.html");
+ //echo "Bienvenido:" .$usuario;
+}
+
+else
 {
     //header("Location: Index.html");
     echo "No ingreso";
