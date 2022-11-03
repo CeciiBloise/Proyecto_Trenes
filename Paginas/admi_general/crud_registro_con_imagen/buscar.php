d2e1c3c62d0b638dc81411fbd5235db6d902a4aa
CeciiBloise <bloisececii@gmail.com>
2022-11-03 11:10:55 -0300

    estoy haciendo una barra de buscador, me falta hacer la conexion con la base de datos

--- Paginas/admi_general/crud_registro_con_imagen/buscar.php
+++ Paginas/admi_general/crud_registro_con_imagen/buscar.php
@@ -1,31 +1,10 @@
 <?php
+include("conexion_crud_registro_con_imagen.php");
+$conexion=conectar();
 
 
-// DEBO PREPARAR LOS TEXTOS QUE VOY A BUSCAR si la cadena existe
-if ($busqueda<>''){
 
-  //CUENTA EL NUMERO DE PALABRAS
-  $trozos=explode(" ",$busqueda);
-  $numero=count($trozos);
-  if ($numero==1) {
+/*https://www.jose-aguilar.com/blog/search-simple-php/ */
 
-    //SI SOLO HAY UNA PALABRA DE BUSQUEDA SE ESTABLECE UNA INSTRUCION CON LIKE
-    $cadbusca="SELECT  REFERENCIA, TITULO FROM ARTICULOS WHERE VISIBLE =1
-      AND DESARROLLO LIKE  '%$busqueda%' OR TITULO LIKE  '%$busqueda%' LIMIT 50";
-  } elseif ($numero>1) {
-    
-    //SI HAY UNA FRASE SE UTILIZA EL ALGORTIMO DE BUSQUEDA AVANZADO DE MATCH AGAINST
-    //busqueda de frases con mas de una palabra y un algoritmo especializado
-    $cadbusca="SELECT  REFERENCIA, TITULO, MATCH ( TITULO, DESARROLLO )
-      AGAINST (  '$busqueda' ) AS Score FROM ARTICULOS WHERE
-      MATCH ( TITULO, DESARROLLO ) AGAINST (  '$busqueda' ) ORDER  BY Score DESC LIMIT 50";
-  }
-  $result=mysql("teleformacion", $cadbusca);
-  While($row=mysql_fetch_object($result))
-  {
-    //Mostramos los titulos de los articulos o lo que deseemos...
-    $referencia=$row->REFERENCIA;
-    $titulo=$row->TITULO;
-    echo $referencia." - ".$titulo."<br>";
-  }
+
 ?>
