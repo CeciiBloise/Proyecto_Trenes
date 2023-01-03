a6db35032c64b598f1d48ed37c2c610e303f30d7
CeciiBloise <bloisececii@gmail.com>
2023-01-03 00:10:07 -0300

    crea la carpeta, guarda los archivos en dicha carpeta y guarda url en la bd de un solo archivo

--- Paginas/admi_general/mecanico/manuales2/insertar_bd2.php
+++ Paginas/admi_general/mecanico/manuales2/insertar_bd2.php
@@ -1,5 +1,5 @@
 <?php
-/*Crea la carpeta subiendo los archivos a esa que creo, y guarda la url en la base de datos*/
+/*Crea la carpeta subiendo los archivos a esa que creo, y guarda la url en la base de datos de un solo archivo*/
 /*https://www.youtube.com/watch?v=bNeT6A2qmQg&t=704s*/
 include("conexion_manuales.php");
 
@@ -7,31 +7,26 @@ $conexion=conectar();
 
 if(isset($_POST['subir'])){
     
-    $manual=$_FILES['manuales'];
-
-    foreach($manual['tmp_name'] as $key => $value){
-        if(file_exists($manual['tmp_name'][$key])){
-            
-            $carpeta=htmlspecialchars($_POST['carpeta']); 
-            $destino="pdf_manuales/";//carpeta de almacenamiento
-            $directorio=$destino.$carpeta;//carpeta que contiene los manuales
-
-            if(!file_exists($directorio)){
-                mkdir($directorio, 0777);//crea el directorio
-                if(move_uploaded_file($manual['tmp_name'][$key],$directorio."/".$manual['name'][$key])){
+    foreach($_FILES['manuales']['tmp_name'] as $key => $value){
         
-                    $ruta=$directorio."/".$manual['name'][$key];
-                    $sql=$conexion->query("INSERT INTO manuales (url) VALUES ('$ruta')"); //guarda la ruta
-                    header("Location: carga_equipo.php");
-                }
-                else{
-                    echo "ocurrio un error";
+        $carpeta=htmlspecialchars($_POST['carpeta']); 
+        $destino="pdf_manuales/";//carpeta de almacenamiento
+        $directorio=$destino.$carpeta;//carpeta que contiene los manuales
+            
+        if(!file_exists($directorio)){
+            mkdir($directorio, 0777);//crea el directorio
+            if(file_exists($_FILES['manuales']['tmp_name'][$key])){
+                if(move_uploaded_file($_FILES['manuales']['tmp_name'][$key],$directorio."/".$_FILES['manuales']['name'][$key])){
+            
+                    echo "el archivo se ha subido";
+    
+                    $ruta=$directorio."/".$_FILES['manuales']['name'][$key];
+                    $sql=$conexion->query("INSERT INTO manuales (carpeta,url) VALUES ('$carpeta','$ruta')"); //guarda la ruta
+    
                 }
             }
         }
     }
-
-
 }
 
 ?>
--- Paginas/admi_general/mecanico/manuales2/insertar_bd_con_array.php
+++ Paginas/admi_general/mecanico/manuales2/insertar_bd_con_array.php
@@ -8,15 +8,18 @@ include("conexion_manuales.php");
 $conexion=conectar();
 
 if(isset($_POST['subir'])){
-
     foreach($_FILES['manuales']['tmp_name'] as $key => $value){
+        
+        $carpeta=$_POST['carpeta'];
+        
+        
         if(file_exists($_FILES['manuales']['tmp_name'][$key])){
             
             if(move_uploaded_file($_FILES['manuales']['tmp_name'][$key],'pdf_manuales/'.$_FILES['manuales']['name'][$key])){
                 echo "el archivo se ha subido";
                 
                 $ruta="pdf_manuales/".$_FILES['manuales']['name'][$key];
-                $sql=$conexion->query("INSERT INTO manuales (url) VALUES ('$ruta')"); //guarda la ruta
+                $sql=$conexion->query("INSERT INTO manuales (carpeta,url) VALUES ('$carpeta','$ruta')"); //guarda la ruta
             }
         }
     }
