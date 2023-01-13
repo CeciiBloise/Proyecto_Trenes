c249b873f87b2e914ea858eac56a60f494bca073
CeciiBloise <bloisececii@gmail.com>
2023-01-13 09:47:36 -0300

    no anda eliminar

--- Paginas/admi_general/mecanico/manuales/eliminar.php
+++ Paginas/admi_general/mecanico/manuales/eliminar.php
@@ -13,24 +13,6 @@ $sql="SELECT * FROM manuales where carpeta='$id'";
 $query= mysqli_query($conexion,$sql);
 
 $row=mysqli_fetch_array($query);
-//elimina la carpeta de la base de datos
-$sql="DELETE FROM manuales WHERE id_manuales='$id'";
-//$query=mysqli_query($conexion,$sql);
-// https://underc0de.org/foro/dudas-generales-121/(solucionado)-eliminar-carpeta-con-contenido-desde-php/
-
-//Elimina la carpeta del servidor pero no se eliminan de la bd de datos
-$carpeta=$row['carpeta'];
-$mis_fotos = "pdf_manuales/".$carpeta;     
-
-foreach(glob($mis_fotos."/*.*") as $archivos_carpeta) 
-
-{ 
- unlink($archivos_carpeta);
- //$sql="DELETE FROM manuales WHERE carpeta='$id'";
- //$query= mysqli_query($conexion,$sql);   
-} 
-//rmdir($mis_fotos);
-header("Location: ver.php");    
 
 ?>
 
