ea4eccab764809db90f7b7b0ac0dabb4e944b6f1
CeciiBloise <bloisececii@gmail.com>
2023-01-13 10:52:12 -0300

    ahora me crea la carpeta lo mueve y solo guarda en la base de datos el nombre de la carpeta

--- Paginas/admi_general/mecanico/manuales/eliminar.php
+++ Paginas/admi_general/mecanico/manuales/eliminar.php
@@ -9,10 +9,45 @@ $conexion=conectar();
 
 $id=$_GET['id'];
 
-$sql="SELECT * FROM manuales where carpeta='$id'";
+$sql="SELECT carpeta FROM manuales";
 $query= mysqli_query($conexion,$sql);
 
 $row=mysqli_fetch_array($query);
+
+function eliminar_directorio_recursivamente($ruta)
+{
+    if (is_dir($ruta)) {
+
+        #Obtener un arreglo con directorios y archivos
+        $subdirectorios_o_archivos = scandir($ruta);
+        foreach ($subdirectorios_o_archivos as $subdirectorio_o_archivo) {
+
+            # Omitir . y .., pues son directorios
+            # que se refieren al directorio actual
+            # o al directorio padre
+            if ($subdirectorio_o_archivo != "." && $subdirectorio_o_archivo != "..") {
+
+                # Si es un directorio, recursión
+                if (is_dir($ruta . "/" . $subdirectorio_o_archivo)) {
+                    eliminar_directorio_recursivamente($ruta . "/" . $subdirectorio_o_archivo);
+                } else {
+                    # Si es un archivo, lo eliminamos con unlink
+                    unlink($ruta . "/" . $subdirectorio_o_archivo);
+                }
+
+            }
+        }
+        # Al final de todo, el directorio estará vacío
+        # y podremos usar rmdir
+        rmdir($ruta);
+    }
+}
+$carpeta=$row['carpeta'];
+$ruta_directorio =$carpeta."/";
+eliminar_directorio_recursivamente($ruta_directorio);
+header("Location: lista.php");
+
+
 
 ?>
 
---
+++ Paginas/admi_general/mecanico/manuales/eliminar2.php
@@ -0,0 +1,7 @@
+<?php
+include("conexion_manuales.php");
+$conexion=conectar();
+
+$sql="DELETE FROM manuales WHERE carpeta=$row "
+
+?>
