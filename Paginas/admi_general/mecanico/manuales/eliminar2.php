57d07b8bb3783ea4adff5fd7e8570dd6afcb2216
CeciiBloise <bloisececii@gmail.com>
2023-01-14 12:57:45 -0300

    Elimina los archivos del servidor y la base de datos

--- Paginas/admi_general/mecanico/manuales/eliminar2.php
+++ Paginas/admi_general/mecanico/manuales/eliminar2.php
@@ -4,36 +4,29 @@ $conexion=conectar();
 
 $id=$_GET['id'];
 
-$sql="SELECT *FROM manuales WHERE id_manuales=$id";
+$sql="SELECT * FROM manuales WHERE id_manuales=$id";
 
 $query= mysqli_query($conexion,$sql);
 
 $row=mysqli_fetch_array($query);
 
 $carpeta=$row['carpeta'];
-$dirname="pdf_manuales/".$carpeta;
-function borrar_directorio($dirname) {
-	//si es un directorio lo abro
-         if (is_dir($dirname))
-           $dir_handle = opendir($dirname);
-        //si no es un directorio devuelvo false para avisar de que ha habido un error
-	 if (!$dir_handle)
-	      return false;
-        //recorro el contenido del directorio fichero a fichero
-	 while($file = readdir($dir_handle)) {
-	       if ($file != "." && $file != "..") {
-                   //si no es un directorio elemino el fichero con unlink()
-	            if (!is_dir($dirname."/".$file))
-	                 unlink($dirname."/".$file);
-	            else //si es un directorio hago la llamada recursiva con el nombre del directorio
-	                 borrar_directorio($dirname.'/'.$file);
-	       }
-	 }
-	 closedir($dir_handle);
-	//elimino el directorio que ya he vaciado
-	 rmdir($dirname);
-     header("Location: lista.php");
-	 return true;
+
+$ruta="pdf_manuales/$carpeta";
+/*$files = glob("$carpeta/*"); //obtenemos todos los nombres de los ficheros
+foreach($files as $file){
+    if(is_file($file)){
+	    unlink($file); //elimino el fichero
+	}
+}*/
+
+$mis_fotos = $ruta;     
+
+foreach(glob("$mis_fotos/*") as $archivos_carpeta){
+	unlink($archivos_carpeta);   
 }
+rmdir($ruta);
+$sql="DELETE FROM manuales  WHERE id_manuales='$id'";
+$query=mysqli_query($conexion,$sql);
 header("Location: lista.php");
 ?>
