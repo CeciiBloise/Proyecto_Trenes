8123fdbfddd5cc32883445317e6ca1374372e6f8
CeciiBloise <bloisececii@gmail.com>
2023-01-20 16:04:29 -0300

    Rehaciendo la seccion de registros

--- Paginas/admi_general/mecanico/registros/tren_universitario/registros_tu/pan_tu/carga_pan_tu.php
+++ Paginas/admi_general/mecanico/registros/tren_universitario/registros_tu/pan_tu/carga_pan_tu.php
@@ -1,8 +1,10 @@
+<!DOCTYPE html> 
+<html lang="es"> 
 <head>
         <meta charset="utf-8" /> <!-- tipos de caracter -->
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-        <link rel="stylesheet" href="../../../../../CSS/estilo_menu_horizontal.css"/>
-        <link rel="stylesheet" href="../../../../../CSS/estilo_registro.css"/>
+        <link rel="stylesheet" href="../../../../../../../CSS/estilo_menu_horizontal.css"/>
+        <link rel="stylesheet" href="../../../../../../../CSS/estilo_registro.css"/>
 
         <title>Trenes Argentinos</title> <!-- titulo de la pagina -->
     
@@ -17,25 +19,18 @@
     </style>
     <header>
       <nav class="navMenu">
-        <li><a href="inicio_tu.php">registros tu</a></li>
+        <li><a href="inicio_pan_tu.php">PAN</a></li>
         <li><a href="../../../../../logout.php">cerrar sesion</a></li>
       </nav>
     </header>
     <body>
         <div class="form_carga">
-            <form action="insertar_bd_tu_año.php" method="POST" class="form">    
-              <h1 class="titulo">Crear Un Año Nuevo</h1>
-
+            <div class="form_carga">
+            <form action="insertar_bd_pan_tu.php" method="POST" class="form">    
+              <h1 class="titulo">Cargar PAN Nuevo - TU</h1>
                 <div class="inputContainer">
-                  <label>Año:</label>
-                  <?php
-                    $cont = date('Y');
-                  ?>
-                  <select name="tu_año" id="sel1">
-                   <?php while ($cont >= 1950) { ?>
-                   <option value="<?php echo($cont); ?>"><?php echo($cont); ?></option>
-                   <?php $cont = ($cont-1); } ?>
-                  </select>  
+                  <label>Paso a Nivel:</label>
+                  <input type="text" name="pan_tu">
                 </div>
                 <div class="boton">
                     <input class="boton-subir" type="submit"  value="subir" name="subir">
---
+++ Paginas/admi_general/mecanico/registros/tren_universitario/registros_tu/pan_tu/ver_pan_año_tu.php
@@ -0,0 +1,68 @@
+<?php
+/*Me lista el contenido de la carpeta*/
+include("../../../conexion_registros.php");
+$conexion=conectar();
+
+$id=$_GET['id'];
+
+$sql="SELECT * FROM registros_tu_pan where id_pan_tu='$id'";
+$query= mysqli_query($conexion,$sql);
+
+$row=mysqli_fetch_array($query);
+
+$listar=null;
+$carpeta_pan=$row['pan_tu'];
+$carpeta_año=$row['pan_año_tu'];
+$directorio=opendir("$carpeta_pan/$carpeta_año");
+$url="$carpeta_pan/$carpeta_año";
+
+while($elemento=readdir($directorio)){
+    if($elemento != '.' && $elemento != '..'){
+        if(is_dir($directorio.$elemento)){
+            $listar .="<ul><a href='$url/$elemento' target='_parent'>$elemento</a></ul><br/>";
+        }else{
+            //error_reporting(0);
+            $listar .="<ul><a href='$url/$elemento' target='_parent'>$elemento</a></ul><br/>";   
+        }
+    }
+}
+?>
+<!DOCTYPE html> 
+<html lang="es"> 
+    <head>
+        <meta charset="utf-8" /> <!-- tipos de caracter -->
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        
+        <!-- Estilos -->
+        <link rel="stylesheet" href="../../../../CSS/listaPdf.css"/>
+        <link rel="stylesheet" href="../../../../CSS/estilo_menu_horizontal.css"/>
+        
+        <title>Trenes Argentinos</title> <!-- titulo de la pagina -->
+    </head>
+
+    <style>
+    
+    </style>
+
+    <header>
+      <nav class="navMenu">
+      <li><a href="" >Inicio</a></li>
+            
+            <li><a href="../../../../logout.php" >Cerrar Sesion</a></li>
+      </nav>
+    </header>
+    <body>
+    <table class="content-table">
+        <thead>
+            <tr>
+                <th><?php echo $row['pan_tu'] ?></th>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td><?php echo $listar?></td>
+        </tr>
+        </tbody>
+        </table>   
+    </body>
+</html>
